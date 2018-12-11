f5f6a071e632225b3504a741a151c47f8e07ee8b
Kirill Kozinets <kirillkozinec123@gmail.com>
2018-12-11 19:49:59 +0700

    Удален лишний код

--- app/src/main/java/com/example/englen/Layouts/MainActivity.java
+++ app/src/main/java/com/example/englen/Layouts/MainActivity.java
@@ -1,41 +1,29 @@
 package com.example.englen.Layouts;
 
-
-import android.content.BroadcastReceiver;
-import android.content.pm.ActivityInfo;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-
 import com.example.englen.Interface.OnBackPressedListener;
 import com.example.englen.Interface.chandgeFragment;
-import com.example.englen.Interface.stateApp;
 import com.example.englen.R;
-import com.example.englen.utils.ExperienceControl;
 
 public class MainActivity extends AppCompatActivity implements chandgeFragment {
 
     Fragment youFragment;
-    stateApp start;
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        start.Close();
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         
 
-        start = new ExperienceControl();
-        start.Create();
 
         if (savedInstanceState == null) {
             youFragment = new MainFragment();
