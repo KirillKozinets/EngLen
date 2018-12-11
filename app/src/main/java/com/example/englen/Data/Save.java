2b9bf700987010258a3176c799c7e184da7b53c6
Kirill Kozinets <kirillkozinec123@gmail.com>
2018-12-11 19:48:03 +0700

    Перегружен метод load

--- app/src/main/java/com/example/englen/Data/Save.java
+++ app/src/main/java/com/example/englen/Data/Save.java
@@ -7,13 +7,18 @@ public class Save {
 
     static SharedPreferences  myPreferences;
 
+    // Сохраняет информацию
     public static void  Save(String Key , String value) {
         SharedPreferences.Editor editor = myPreferences.edit();
         editor.putString(Key,value);
         editor.apply();
     }
 
+    // Читает информацию
     public static String load(String Key) {
         return myPreferences.getString(Key, "0");
+    }
+    public static String load(String Key,String defaultValue) {
+        return myPreferences.getString(Key, defaultValue);
     }
 }
