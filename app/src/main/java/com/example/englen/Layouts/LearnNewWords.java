186714fd85843eb3305ded7f8b1453d5e4b9b5b6
Kirill Kozinets <kirillkozinec123@gmail.com>
2018-12-11 19:49:12 +0700

    Добавлен комментарий

--- app/src/main/java/com/example/englen/Layouts/LearnNewWords.java
+++ app/src/main/java/com/example/englen/Layouts/LearnNewWords.java
@@ -56,6 +56,7 @@ public class LearnNewWords extends Fragment implements chandgeTaskAnswer, OnBack
         View view = inflater.inflate(R.layout.fragment_learn_new_words, container, false);
         Button but = view.findViewById(R.id.butBack);
 
+        // Выполняется при нажатие кнопки назад
         but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
