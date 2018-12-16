7e5db8e9a26259b774490f87f582756e3a4c47e1
Kirill Kozinets <kirillkozinec123@gmail.com>
2018-12-16 15:59:44 +0700

    Изменён способ загрузки информации из БД

--- app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
+++ app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.englen.Data.DataBase.DataBaseHelper;
+import com.example.englen.Layouts.LearnNewWords;
 import com.example.englen.utils.LearnWord;
 
 import java.io.IOException;
@@ -11,29 +12,34 @@ import java.util.Random;
 
 public class ReadTask {
 
-    // Читай из базы данных запись с определённым номером 
-    public static String[] readTask(DataBaseHelper helper, int item, String type) throws Exception {
-        String[] ArraysResult = new String[item];
+    static int MaxWord;
 
+    // Читай из базы данных запись с определённым номером 
+    public static String[] readTask(DataBaseHelper helper, int item, String type, int max, Boolean isNew) throws Exception {
+        String[] ArraysResult = new String[item];
+        updataDataBase(helper);// Обновляем базу данных
+        SQLiteDatabase mDb = helper.getWritableDatabase();// Читаем базу данных
+
+        Cursor cursor = mDb.rawQuery("SELECT * FROM TaskAnswersList WHERE Type = type ", null); // Читаем из базы данных определенные записи
+        if (cursor.getCount() > max && (LearnWord.getCurrentID() > max || isNew == true)) {// Если мы не дошли до последней записи
+            // Читаем запись
+            cursor.move(max);
+            for (int i = 0; i < item; i++) {
+                ArraysResult[i] = cursor.getString(i + 1);
+            }
+
+        } else
+            throw new Exception();
+
+        return ArraysResult;
+    }
+
+    private static void updataDataBase(DataBaseHelper helper) {
         try {
             helper.updateDataBase();
         } catch (IOException mIOException) {
             throw new Error("UnableToUpdateDatabase");
         }
-
-        SQLiteDatabase mDb = helper.getWritableDatabase();
-
-        Cursor cursor = mDb.rawQuery("SELECT * FROM TaskAnswersList WHERE Type = type ", null);
-        if(cursor.getCount() > LearnWord.getCurrentID()) {
-            cursor.move(LearnWord.getCurrentID());
-
-            for (int i = 0; i < item; i++)
-                ArraysResult[i] = cursor.getString(i + 1);
-        }
-        else
-            throw new Exception();
-
-        return ArraysResult;
     }
 
 }
