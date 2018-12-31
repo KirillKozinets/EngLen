3e76cd942d7ee822f53f27347d96eb86e5861233
Kirill Kozinets <kirillkozinec123@gmail.com>
2018-12-31 22:30:28 +0700

    Переименован

--- app/src/main/java/com/example/englen/Interface/ChandgeFragment.java
+++ app/src/main/java/com/example/englen/Interface/ChandgeFragment.java
@@ -2,6 +2,6 @@ package com.example.englen.Interface;
 
 import android.support.v4.app.Fragment;
 
-public interface chandgeFragment {
+public interface ChandgeFragment {
     void onCloseFragment(Fragment fragment);
 }
