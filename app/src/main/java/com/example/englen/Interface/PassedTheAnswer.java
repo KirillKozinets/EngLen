5afc91b0c402926c9379dc9a34fb0f380f85be61
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-01-04 10:40:58 +0700

    Добавлен интерфейс

---
+++ app/src/main/java/com/example/englen/Interface/PassedTheAnswer.java
@@ -0,0 +1,8 @@
+package com.example.englen.Interface;
+
+import android.support.annotation.Nullable;
+
+public interface PassedTheAnswer {
+    void PassedTheAnswer(@Nullable boolean trueAnswer);
+    void Exit();
+}
