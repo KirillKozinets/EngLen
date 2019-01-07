52d71e7572ca6c9ba6178464c42e338172bb375f
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-01-07 15:12:10 +0700

    Изменён метод для чтения информации из БД

--- app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
+++ app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
@@ -3,24 +3,21 @@ package com.example.englen.Data.DataBase;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.example.englen.Data.DataBase.DataBaseHelper;
-import com.example.englen.Layouts.LearnNewWords;
 import com.example.englen.utils.LearnWord;
 
 import java.io.IOException;
-import java.util.Random;
 
 public class ReadTask {
 
     static int MaxWord;
 
     // Читай из базы данных запись с определённым номером 
-    public static String[] readTask(DataBaseHelper helper, int item, String type, int max, Boolean isNew) throws Exception {
+    public static String[] readTask(DataBaseHelper helper, int item, int max, Boolean isNew) throws Exception {
         String[] ArraysResult = new String[item];
         updataDataBase(helper);// Обновляем базу данных
         SQLiteDatabase mDb = helper.getWritableDatabase();// Читаем базу данных
 
-        Cursor cursor = mDb.rawQuery("SELECT * FROM TaskAnswersList WHERE Type = type ", null); // Читаем из базы данных определенные записи
+        Cursor cursor = mDb.rawQuery("SELECT * FROM TaskAnswersList", null); // Читаем из базы данных определенные записи
         if (cursor.getCount() > max && (LearnWord.getCurrentID() > max || isNew == true)) {// Если мы не дошли до последней записи
             // Читаем запись
             cursor.move(max);
@@ -34,7 +31,7 @@ public class ReadTask {
         return ArraysResult;
     }
 
-    private static void updataDataBase(DataBaseHelper helper) {
+    public static void updataDataBase(DataBaseHelper helper) {
         try {
             helper.updateDataBase();
         } catch (IOException mIOException) {
