229e931e4bf53249d635ec2a82a00b5544c164a9
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-01-07 18:27:07 +0700

    Добавлен метод для чтения взей базы данных

--- app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
+++ app/src/main/java/com/example/englen/Data/DataBase/ReadTask.java
@@ -9,9 +9,7 @@ import java.io.IOException;
 
 public class ReadTask {
 
-    static int MaxWord;
-
-    // Читай из базы данных запись с определённым номером 
+    // Читай из базы данных запись с определённым номером
     public static String[] readDataFromBD(DataBaseHelper helper, int ID , String BDName) throws Exception {
         updataDataBase(helper);// Обновляем базу данных
         SQLiteDatabase mDb = helper.getWritableDatabase();// Читаем базу данных
@@ -21,13 +19,42 @@ public class ReadTask {
         return readFromBD(ID,cursor,size);
     }
 
+    // Читай из базы данных запись с определённым номером
+    public static String[][] readAllDataFromBD(DataBaseHelper helper , String BDName) {
+        updataDataBase(helper);// Обновляем базу данных
+        SQLiteDatabase mDb = helper.getWritableDatabase();// Читаем базу данных
+        Cursor cursor = mDb.rawQuery("SELECT * FROM " + BDName, null); // Читаем из базы данных определенные записи
+        int size =cursor.getColumnCount();
+        String Result[][] = new String[cursor.getCount()][cursor.getColumnCount()];
+
+        for(int i = 0 ; i < cursor.getCount() ; i++)
+        {
+            String[] temp = new String[0];
+
+            try {
+                temp = readFromBD(i,cursor,size);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            for(int q = 0 ; q < cursor.getColumnCount() ; q++)
+            {
+                Result[i][q] = temp[q];
+            }
+        }
+
+        return Result;
+    }
+
     private static String[] readFromBD(int ID , Cursor cursor , int size) throws Exception {
         String ArraysResult[] = new String[size];
         if (cursor.getCount() > ID) {// Если мы не дошли до последней записи
             // Читаем запись
             cursor.move(ID);
-            for (int i = 0; i < size; i++) {
-                ArraysResult[i] = cursor.getString(i + 1);
+            if(cursor.moveToFirst()) {
+                for (int i = 1; i < size; i++) {
+                    ArraysResult[i - 1] = cursor.getString(i);
+                }
             }
         } else
             throw new Exception();
