22ba5a5d835e310636e8d7c8a201f8450ff7ca95
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-02 13:36:04 +0700

    Create ItemTheory.java

---
+++ app/src/main/java/com/example/englen/utils/ItemTheory.java
@@ -0,0 +1,19 @@
+package com.example.englen.utils;
+
+public class ItemTheory {
+
+    private String text;
+
+
+    public ItemTheory(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
