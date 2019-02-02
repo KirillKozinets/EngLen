67d4bd1a2fbd3730e1d2ba40dfa7b9ad231b52a1
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-02 18:44:44 +0700

    Запоминает id

--- app/src/main/java/com/example/englen/utils/ItemTheory.java
+++ app/src/main/java/com/example/englen/utils/ItemTheory.java
@@ -3,10 +3,12 @@ package com.example.englen.utils;
 public class ItemTheory {
 
     private String text;
+    private int id;
 
 
-    public ItemTheory(String text) {
+    public ItemTheory(String text , int id) {
         this.text = text;
+        this.id = id;
     }
 
     public String getText() {
@@ -15,5 +17,13 @@ public class ItemTheory {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
