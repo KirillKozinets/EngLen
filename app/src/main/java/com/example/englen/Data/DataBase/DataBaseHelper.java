2145afdbc6f41fc7104bade1526f4a98f695f2cf
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-09 11:36:41 +0700

    Обновлена БД

--- app/src/main/java/com/example/englen/Data/DataBase/DataBaseHelper.java
+++ app/src/main/java/com/example/englen/Data/DataBase/DataBaseHelper.java
@@ -15,7 +15,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     private static String DB_NAME = "TaskAnswers.db";
     private static String DB_PATH = "";
-    private static final int DB_VERSION = 26;
+    private static final int DB_VERSION = 28;
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
