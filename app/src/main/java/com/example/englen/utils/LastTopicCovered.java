cb07627b468c11acf70af38ea4805b6318b15560
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-09 11:37:19 +0700

    Добавлен класс для хранения последней пройденой темы

---
+++ app/src/main/java/com/example/englen/utils/LastTopicCovered.java
@@ -0,0 +1,29 @@
+package com.example.englen.utils;
+
+import android.content.Context;
+
+import com.example.englen.Data.Save;
+
+public class LastTopicCovered {
+    static private int lastTopicCoveredID = 0;
+
+    private static final String tagID = "taglastTopicCoveredID";
+
+    static public int getlastTopicCoveredID()
+    {
+        return lastTopicCoveredID;
+    }
+
+    static public void setlastTopicCoveredID(int value)
+    {
+        lastTopicCoveredID = value;
+    }
+
+    public static void Save() {
+        Save.Save(tagID, Integer.toString(lastTopicCoveredID));
+    }
+
+    public static void Load(Context context) {
+        lastTopicCoveredID = Integer.parseInt(Save.load(tagID, "0",context));
+    }
+}
