51184d5ae3977e0363424d01035fd0e677951fd9
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-09 11:36:59 +0700

    Изменено

--- app/src/main/java/com/example/englen/utils/ItemTheory.java
+++ app/src/main/java/com/example/englen/utils/ItemTheory.java
@@ -4,11 +4,12 @@ public class ItemTheory {
 
     private String text;
     private int id;
+    private boolean active;
 
-
-    public ItemTheory(String text , int id) {
+    public ItemTheory(String text, int id, boolean active) {
         this.text = text;
         this.id = id;
+        this.active = active;
     }
 
     public String getText() {
@@ -25,5 +26,13 @@ public class ItemTheory {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
