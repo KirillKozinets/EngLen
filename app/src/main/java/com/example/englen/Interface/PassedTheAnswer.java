8f49ac50d8a7ea92f696dc38999bf082ce876876
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-11 17:00:30 +0700

    Изменён итерфейс

--- app/src/main/java/com/example/englen/Interface/PassedTheAnswer.java
+++ app/src/main/java/com/example/englen/Interface/PassedTheAnswer.java
@@ -3,6 +3,6 @@ package com.example.englen.Interface;
 import android.support.annotation.Nullable;
 
 public interface PassedTheAnswer {
-    void PassedTheAnswer(@Nullable boolean trueAnswer);
+    void PassedTheAnswer(@Nullable boolean trueAnswer , int progress);
     void Exit();
 }
