d3073a83ec8d3436209212778f0595c8856e0647
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-02-11 17:03:05 +0700

    Исправлен баг

--- app/src/main/java/com/example/englen/view/Fragments/Theory.java
+++ app/src/main/java/com/example/englen/view/Fragments/Theory.java
@@ -1,16 +1,26 @@
 package com.example.englen.view.Fragments;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 
 import com.example.englen.Interface.ChandgeFragment;
 import com.example.englen.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Theory extends Fragment {
@@ -60,16 +70,37 @@ public class Theory extends Fragment {
         WebView htmlView = view.findViewById(R.id.theory);
         htmlView.getSettings().setJavaScriptEnabled(true);
 
-        htmlView.loadDataWithBaseURL("", HTML, "text/html", "UTF-8", "");
+        WebSettings settings = htmlView.getSettings();
+        settings.setDefaultTextEncodingName("UTF-8");
+        htmlView.loadDataWithBaseURL(null,
+                getText("HTML/" +HTML),
+                "text/html; charset=utf-8",
+                "UTF-8",null);
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CF.onCloseFragment(TestTheory.newInstance(DBname,id));
+                CF.onCloseFragment(TestTheory.newInstance(DBname, id));
             }
         });
 
 
         return view;
+    }
+
+    private String getText(String fileName) {
+        String xmlString = null;
+        AssetManager am = getContext().getAssets();
+        try {
+            InputStream is = am.open(fileName);
+            int length = is.available();
+            byte[] data = new byte[length];
+            is.read(data);
+            xmlString = new String(data);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return xmlString;
     }
 
     @Override
