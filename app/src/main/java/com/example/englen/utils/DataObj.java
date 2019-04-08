a0b7fb119a024a7a33bd1537830b2c7614ca425c
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-04-08 17:34:05 +0700

    Добавлен адаптер для вывода списка изученных слов

---
+++ app/src/main/java/com/example/englen/utils/DataObj.java
@@ -0,0 +1,11 @@
+package com.example.englen.utils;
+
+public class DataObj {
+    String field1;
+    String field2;
+
+    public DataObj(String a, String b) {
+        field1 = a;
+        field2 = b;
+    }
+}
---
+++ app/src/main/java/com/example/englen/utils/SearchResultAdapter.java
@@ -0,0 +1,40 @@
+package com.example.englen.utils;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.englen.R;
+
+import java.util.ArrayList;
+
+
+public class SearchResultAdapter extends ArrayAdapter<DataObj> {
+
+    public SearchResultAdapter(Context context, ArrayList<DataObj> dataList) {
+        super(context, 0, dataList);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        DataObj data = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.element_layout, parent, false);
+        }
+
+        TextView column1 =  convertView.findViewById(R.id.col1);
+        TextView column2 =  convertView.findViewById(R.id.col2);
+
+
+        column2.setText(data.field1);
+        column1.setText(data.field2);
+
+
+        return convertView;
+    }
+}
