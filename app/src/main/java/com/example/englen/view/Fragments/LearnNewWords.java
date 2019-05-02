52ee5f67600bca5ace18f4da8550b799f083e599
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 13:20:59 +0700

    Убрана кнопка для возвращения назад

--- app/src/main/java/com/example/englen/view/Fragments/LearnNewWords.java
+++ app/src/main/java/com/example/englen/view/Fragments/LearnNewWords.java
@@ -78,15 +78,6 @@ public class LearnNewWords extends Fragment implements ChandgeTaskAnswer, OnBack
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_learn_new_words, container, false);
-        Button but = view.findViewById(R.id.butBack);
-
-        // Выполняется при нажатие кнопки назад
-        but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Back();
-            }
-        });
 
         return view;
     }
