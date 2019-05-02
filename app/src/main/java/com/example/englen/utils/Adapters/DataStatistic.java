3e06b3c602df701853349019c14be8ab5e023027
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 20:52:10 +0700

    Добавлен класс для хранения элементов списка со статистикой

---
+++ app/src/main/java/com/example/englen/utils/Adapters/DataStatistic.java
@@ -0,0 +1,13 @@
+package com.example.englen.utils.Adapters;
+
+public class DataStatistic {
+    Object field1;
+    Object field2;
+    Object field3;
+
+    public DataStatistic(Object a, Object b, Object c) {
+        field1 = a;
+        field2 = b;
+        field3 = c;
+    }
+}
