68b4fbd206e86c7967b871635adea1f5ad489c87
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 13:21:23 +0700

    Update MainFragment.java

--- app/src/main/java/com/example/englen/view/Fragments/MainFragment.java
+++ app/src/main/java/com/example/englen/view/Fragments/MainFragment.java
@@ -9,9 +9,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.englen.Data.Retrofit.RetrofitNet;
 import com.example.englen.Interface.ChandgeFragment;
 import com.example.englen.Interface.OnBackPressedListener;
 import com.example.englen.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainFragment extends Fragment implements OnBackPressedListener {
 
@@ -34,19 +39,28 @@ public class MainFragment extends Fragment implements OnBackPressedListener {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         Button gram = view.findViewById(R.id.butGram);
         Button button = view.findViewById(R.id.NewWord);
+        Button buttonTranslate = view.findViewById(R.id.translate);
 
         gram.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { Fragment.onCloseFragment(LearnG
-            );
+            public void onClick(View view) {
+                Fragment.onCloseFragment(LearnG
+                );
             }
         });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              Fragment.onCloseFragment(word_list.newInstance());
+                Fragment.onCloseFragment(word_list.newInstance());
             }
         });
+        buttonTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment.onCloseFragment(TranslateFragment.newInstance());
+            }
+        });
+
 
         return view;
     }
