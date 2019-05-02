d42eb6beac0a12d299d164baa49a9685c94626cb
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 13:55:19 +0700

    Добавлено отображение количества слов , которые нужно повторить

--- app/src/main/java/com/example/englen/view/Fragments/TaskAnswer/TaskAnswerFragmentRememberWord.java
+++ app/src/main/java/com/example/englen/view/Fragments/TaskAnswer/TaskAnswerFragmentRememberWord.java
@@ -6,11 +6,13 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.englen.Data.DataBase.DataBaseHelper;
 import com.example.englen.Data.DataBase.ReadFromDataBase;
 import com.example.englen.Interface.LeanWord;
+import com.example.englen.R;
 import com.example.englen.utils.Fabric;
 import com.example.englen.utils.LearnWord;
 
@@ -20,6 +22,8 @@ public class TaskAnswerFragmentRememberWord extends TaskAnswerFragment {
 
     Random random = new Random();
     int rand;
+    private TextView text;
+    private int rememberWord = 0;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,21 +45,29 @@ public class TaskAnswerFragmentRememberWord extends TaskAnswerFragment {
     //Читает информацию из базы данных
     protected boolean ReadBD(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
+            if (text != null) {
+                rememberWord++;
+                text.setVisibility(View.VISIBLE);
+                text.setText(rememberWord + " / 10");
+            }
+            if (rememberWord >= 10) {
+                LeanWord cF = (LeanWord) getParentFragment();
+                cF.LeanWord(); // Закрывает текущий фрагмент и показывает информацию о уровне
+            }
             mDBHelper = new DataBaseHelper(getActivity());
             try {
-                if (LearnWord.getCurrentID() > 50) {
+                if (LearnWord.getCurrentID() > 10) {
                     rand = random.nextInt(LearnWord.getCurrentID());
                     Result = ReadFromDataBase.readDataFromBD(mDBHelper, rand, "TaskAnswersList"); // Читает из бызы данных записи
-                }
-                else
+                } else
                     throw new ArrayIndexOutOfBoundsException();
             } catch (Exception ex) {
-                    Toast toast = Toast.makeText(getContext(),
-                            "Нужно выучить больше слов",
-                            Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getContext(),
+                        "Нужно выучить больше слов",
+                        Toast.LENGTH_SHORT);
 
-                    toast.setGravity(Gravity.CENTER, 0, 0);
-                    toast.show();
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
                 return false;
             }
         } else {
@@ -70,9 +82,13 @@ public class TaskAnswerFragmentRememberWord extends TaskAnswerFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (ReadBD(savedInstanceState)) {
-            return super.onCreateView(inflater, container, savedInstanceState);
-        }
+            View view = super.onCreateView(inflater, container, savedInstanceState);
+            text = view.findViewById(R.id.listWord);
+            text.setVisibility(View.VISIBLE);
+            text.setText(rememberWord + " / 10");
+            return view;
 
+        }
         LeanWord cF = (LeanWord) getParentFragment();
         cF.LeanWord();
 
@@ -83,10 +99,10 @@ public class TaskAnswerFragmentRememberWord extends TaskAnswerFragment {
     protected void TrueAndFalseAnswer() {
         super.TrueAndFalseAnswer();
         int id = rand;
-        if (userAnsver + 1 == trueAnswer)
-            Fabric.enterLevel("RememberWord_"+id, 100, true);
-        else
-            Fabric.enterLevel("RememberWord_"+id, 0, false);
+        if (userAnsver + 1 == trueAnswer) {
+            Fabric.enterLevel("RememberWord_" + id, 100, true);
+        } else
+            Fabric.enterLevel("RememberWord_" + id, 0, false);
     }
 
 }
