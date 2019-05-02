d33245c5ea92198201b855cbdfb7567dc1ae6ffd
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 13:19:48 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/englen/view/Activitis/MainActivity.java
+++ app/src/main/java/com/example/englen/view/Activitis/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements ChandgeFragment {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
         Fabric.with(getApplicationContext(), new Crashlytics());
         ExperienceControl.Load(getApplicationContext());
