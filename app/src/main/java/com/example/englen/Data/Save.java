4953d7f4d5c6447e96f9b472175076700ccdae3c
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 20:51:38 +0700

    Update Save.java

--- app/src/main/java/com/example/englen/Data/Save.java
+++ app/src/main/java/com/example/englen/Data/Save.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 public class Save {
 
 
-    static SharedPreferences  myPreferences;
+        static SharedPreferences  myPreferences;
 
     static private final String FileName = "File";
 
