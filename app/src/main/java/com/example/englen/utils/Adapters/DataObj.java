e37e7e3771c3e0cd4ef45e4aad1987edf4c8b406
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-02 20:51:49 +0700

    Перемещен

---
+++ app/src/main/java/com/example/englen/utils/Adapters/DataObj.java
@@ -0,0 +1,11 @@
+package com.example.englen.utils.Adapters;
+
+public class DataObj {
+    Object field1;
+    Object field2;
+
+    public DataObj(Object a, Object b) {
+        field1 = a;
+        field2 = b;
+    }
+}
