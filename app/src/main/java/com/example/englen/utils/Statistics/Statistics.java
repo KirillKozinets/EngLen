475958a973f50636b4bc72294018499becfbc070
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-03 12:41:53 +0700

    Увеличен сбр статистики

--- app/src/main/java/com/example/englen/utils/Statistics/Statistics.java
+++ app/src/main/java/com/example/englen/utils/Statistics/Statistics.java
@@ -4,46 +4,62 @@ import android.content.Context;
 
 import com.example.englen.Data.Save;
 
-import java.util.ArrayList;
-
 public class Statistics {
+    
     private static final String tagHours = "tagHours";
-    private static int hours;//Потраченных часов
+    private static int minutes;//Потраченных часов
     private static final String taglearnWord= "taglearnWord";
     private static int learnWord;//Выученных слов
     private static final String tagRemember = "tagRemember";
     private static int rememberWord;//Повторено слов
     private static final String tagTheme = "tagTheme";
     private static int learnTheme;//Изучено тем
+    private static final String tagFalseRemember = "tagFalseRemember";
+    private static int falseRememberWord;//Не Правильных ответов при повторение слов
+
+    private static int trueRememberWordPercent = 100;
 
     public static void Save()
     {
-        Save.Save(tagHours, Integer.toString(hours));
+        Save.Save(tagHours, Integer.toString(minutes));
         Save.Save(taglearnWord, Integer.toString(learnWord));
         Save.Save(tagRemember, Integer.toString(rememberWord));
         Save.Save(tagTheme, Integer.toString(learnTheme));
+        Save.Save(tagFalseRemember, Integer.toString(falseRememberWord));
     }
 
     public static void Load(Context context)
     {
-        hours = Integer.parseInt(Save.load(tagHours, "0",context));
+        minutes = Integer.parseInt(Save.load(tagHours, "0",context));
         learnWord =Integer.parseInt(Save.load(taglearnWord, "0",context));
         rememberWord = Integer.parseInt(Save.load(tagRemember, "0",context));
         learnTheme = Integer.parseInt(Save.load(tagTheme, "0",context));
+        falseRememberWord = Integer.parseInt(Save.load(tagFalseRemember, "0",context));
     }
 
     public static int[] toArray() {
+        if(rememberWord!=0 && falseRememberWord !=0)
+        {
+            int a = rememberWord - falseRememberWord;
+            float b = (float) a / (float)rememberWord;
+            trueRememberWordPercent = (int)( b * 100);
+        }
         int[] array = new int[]{
-                hours,
+                minutes,
                 learnWord,
                 rememberWord,
-                learnTheme
+                learnTheme,
+                trueRememberWordPercent
         };
         return array;
     }
 
-    public static void addHours(int value) {
-        hours += value;
+    public static void addfalseRememberWord(int value) {
+        falseRememberWord += value;
+    }
+
+    public static void addMinutes(int value) {
+        minutes += value;
     }
 
     public static void addLearnWord(int value) {
