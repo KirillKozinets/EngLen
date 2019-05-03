e3184d8a0309cb285bfca326a8278fa8269ea147
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-05-03 12:42:19 +0700

    Изменено

--- app/src/main/java/com/example/englen/view/Fragments/MainFragment.java
+++ app/src/main/java/com/example/englen/view/Fragments/MainFragment.java
@@ -7,18 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-import com.example.englen.Data.Retrofit.RetrofitNet;
 import com.example.englen.Interface.ChandgeFragment;
 import com.example.englen.Interface.OnBackPressedListener;
 import com.example.englen.R;
 import com.example.englen.utils.Statistics.Statistics;
 import com.example.englen.utils.Statistics.Time;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainFragment extends Fragment implements OnBackPressedListener {
 
@@ -49,7 +43,7 @@ public class MainFragment extends Fragment implements OnBackPressedListener {
             public void onClick(View view) {
                 Time.stopTime = System.currentTimeMillis();
                 float lastTime = (Time.stopTime - Time.startTime);
-                Statistics.addHours((int)(lastTime / 60000));
+                Statistics.addMinutes((int)(lastTime / 60000));
                 Time.startTime = Time.stopTime;
                 Fragment.onCloseFragment(new StatisticsFragment());
             }
